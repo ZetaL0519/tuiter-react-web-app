8133df469fbedb28408d11c8287f48da150f5eec
ZoeLee <zoelee@ZoeLees-MacBook-Pro.local>
2022-10-20 20:53:59 -0700

    a6 React.js sp22

---
+++ src/labs/a6/index.js
@@ -0,0 +1,20 @@
+import Classes from "./classes";
+import Styles from "./styles";
+import TodoList from "./todo/todo-list.js";
+import ConditionalOutput
+  from "./conditional-output";
+import Nav from "../../nav";
+
+function Assignment6() {
+ return (
+   <div>
+     <Nav/>
+     <h1>Assignment 6</h1>
+     <ConditionalOutput/>
+     <TodoList/>
+     <Styles/>
+     <Classes/>
+   </div>
+ );
+}
+export default Assignment6;
